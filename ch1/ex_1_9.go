ae5610040afe596e4ed8eff617b6a262fd0c18ff
Craig Ferry <craig@puritycontrol.net>
2016-01-31 09:41:12 +0000

    Complete exercise 1.9.

---
+++ ch1/ex_1_9.go
@@ -0,0 +1,30 @@
+// Fetch prints the contents of a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "Status: %v\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: error copying from source to destination %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+		
